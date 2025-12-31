8d61b1191fc376e9ed98c22f0a6d181fe27e9ec9
tidjani783 <ahmadtidjanithiam@gmail.com>
2025-12-31 19:14:52 +0000

    crud avis + bouton ajouter

---
+++ src/Controller/AdminAvisController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Avis;
+use App\Form\AvisType;
+use App\Repository\AvisRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminAvisController extends AbstractController
+{
+    #[Route('/admin/avis', name:'admin_avis_liste')]
+    public function liste(AvisRepository $repo): Response
+    {
+        return $this->render('avis/AdminlisteAvis.html.twig', [
+            'lesAvis' => $repo->findAll()
+        ]);
+    }
+
+    #[Route('/admin/avis/ajout', name:'admin_avis_ajout')]
+    public function ajout(Request $request, EntityManagerInterface $em): Response
+    {
+        $avis = new Avis();
+        $form = $this->createForm(AvisType::class, $avis);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($avis);
+            $em->flush();
+            $this->addFlash('success', 'Avis ajouté 👍');
+            return $this->redirectToRoute('admin_avis_liste');
+        }
+
+        return $this->render('avis/AdminformAjoutModifAvis.html.twig', [
+            'formAvis' => $form->createView()
+        ]);
+    }
+
+    #[Route('/admin/avis/modif/{id}', name:'admin_avis_modif')]
+    public function modif(Avis $avis, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(AvisType::class, $avis);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+            $this->addFlash('success', 'Avis modifié ✏️');
+            return $this->redirectToRoute('admin_avis_liste');
+        }
+
+        return $this->render('avis/AdminformAjoutModifAvis.html.twig', [
+            'formAvis' => $form->createView()
+        ]);
+    }
+
+    #[Route('/admin/avis/supp/{id}', name:'admin_avis_supp')]
+    public function supp(Avis $avis, EntityManagerInterface $em): Response
+    {
+        $em->remove($avis);
+        $em->flush();
+        $this->addFlash('danger', 'Avis supprimé 🗑');
+        return $this->redirectToRoute('admin_avis_liste');
+    }
+}
